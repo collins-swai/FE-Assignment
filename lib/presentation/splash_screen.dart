54b5aca0b84eb30e6a17e5881dee8f2db9f46471
Collins Swai <swailerc23@gmail.com>
2023-03-24 22:02:24 +0300

    Splash screen modified

--- lib/presentation/splash_screen.dart
+++ lib/presentation/splash_screen.dart
@@ -1,10 +1,16 @@
 import 'package:acme_corp_interview/presentation/login_screen.dart';
+import 'package:acme_corp_interview/presentation/register_screen.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen(
+      {Key? key, required this.name, required this.email, required this.phone})
+      : super(key: key);
+  final String name;
+  final String email;
+  final String phone;
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -18,7 +24,11 @@ class _SplashScreenState extends State<SplashScreen> {
       home: AnimatedSplashScreen(
         duration: 5000,
         splash: Image.asset("assets/images/logo-dark.png"),
-        nextScreen: LoginScreen(),
+        nextScreen: LoginScreen(
+          name: widget.name,
+          email: widget.email,
+          phone: widget.phone,
+        ),
         splashTransition: SplashTransition.slideTransition,
         pageTransitionType: PageTransitionType.leftToRightWithFade,
       ),
