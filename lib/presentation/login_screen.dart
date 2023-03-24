6afbf7822dd5783b0b2a178f28982041bb2874b9
Collins Swai <swailerc23@gmail.com>
2023-03-24 22:01:13 +0300

    Updated login screen

--- lib/presentation/login_screen.dart
+++ lib/presentation/login_screen.dart
@@ -1,17 +1,310 @@
+import 'package:acme_corp_interview/presentation/Agent/agent_home_screen.dart';
+import 'package:acme_corp_interview/presentation/Client/client_home_screen.dart';
+import 'package:acme_corp_interview/presentation/register_screen.dart';
+import 'package:acme_corp_interview/theme/app_style.dart';
+import 'package:acme_corp_interview/theme/color_constant.dart';
+import 'package:acme_corp_interview/theme/size_utils.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  const LoginScreen(
+      {Key? key, required this.name, required this.email, required this.phone})
+      : super(key: key);
+  final String name;
+  final String email;
+  final String phone;
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool _isHidden = true;
+
+  String _errorMessage = '';
+
+  void _login() {
+    String username = emailController.text;
+    String password = passwordController.text;
+
+    if (username == 'agent' && password == 'password') {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => AgentHomeScreen(
+                  name: widget.name,
+                  email: widget.email,
+                  phone: widget.phone,
+                )),
+      );
+    } else if (username == 'client' && password == 'password') {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => ClientHomeScreen()),
+      );
+    } else {
+      setState(() {
+        _errorMessage = 'Invalid username or password';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    final emailField = TextFormField(
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      textAlignVertical: TextAlignVertical.center,
+      autofocus: false,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'The email field is required.';
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 1, color: ColorConstant.plainGrey),
+          ),
+          errorBorder: const OutlineInputBorder(
+            borderSide: BorderSide(width: 1, color: Colors.red),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 1, color: ColorConstant.teal800),
+          ),
+          label: Text("Email"),
+          contentPadding: EdgeInsets.fromLTRB(14.0, 1.0, 4.0, 2.0),
+          labelStyle: AppStyle.txtInterMedium18.copyWith(
+            letterSpacing: 0.50,
+            height: 1.00,
+          ),
+          filled: true,
+          fillColor: ColorConstant.whiteA700),
     );
+
+    final passwordField = TextFormField(
+      controller: passwordController,
+      textAlignVertical: TextAlignVertical.center,
+      keyboardType: TextInputType.visiblePassword,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "Password";
+        } else if (value.length < 6) {
+          return "Password";
+        }
+        return null;
+      },
+      obscureText: true,
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 1, color: ColorConstant.plainGrey),
+        ),
+        errorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(width: 1, color: Colors.red),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 1, color: ColorConstant.teal800),
+        ),
+        contentPadding: EdgeInsets.fromLTRB(14.0, 1.0, 4.0, 2.0),
+        labelStyle: AppStyle.txtInterMedium20.copyWith(
+          letterSpacing: 0.50,
+          height: 1.00,
+        ),
+        label: Text("Password"),
+        filled: true,
+        errorText: _errorMessage,
+        alignLabelWithHint: true,
+        fillColor: ColorConstant.whiteA700,
+        suffix: Container(
+          padding: getPadding(right: 5, top: 4),
+          child: InkWell(
+            onTap: _togglePasswordView,
+            child: Icon(
+              _isHidden ? Icons.visibility : Icons.visibility_off,
+              color: ColorConstant.lightGray,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: getPadding(left: 24, top: 100, right: 24),
+                    child: Image.asset("assets/images/logo-dark.png"),
+                  ),
+                ),
+                Padding(
+                  padding: getPadding(top: 15),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      height: getVerticalSize(74.00),
+                      width: getHorizontalSize(396.00),
+                      margin: getMargin(left: 16, right: 16, bottom: 4),
+                      child: Stack(
+                        alignment: Alignment.topLeft,
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Container(
+                              height: getVerticalSize(
+                                74.00,
+                              ),
+                              width: getHorizontalSize(
+                                396.00,
+                              ),
+                              margin: getMargin(
+                                top: 10,
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: emailField,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: getPadding(top: 15),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      height: getVerticalSize(74.00),
+                      width: getHorizontalSize(396.00),
+                      margin: getMargin(left: 16, right: 16, bottom: 4),
+                      child: Stack(
+                        alignment: Alignment.topLeft,
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Container(
+                              height: getVerticalSize(
+                                74.00,
+                              ),
+                              width: getHorizontalSize(
+                                396.00,
+                              ),
+                              margin: getMargin(
+                                top: 10,
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: passwordField,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: getPadding(bottom: 2),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _login();
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: StadiumBorder(),
+                            backgroundColor: ColorConstant.teal800,
+                            minimumSize: Size(340, 40),
+                          ),
+                          child: Text("Login"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: getPadding(top: 15),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      height: getVerticalSize(74.00),
+                      width: getHorizontalSize(396.00),
+                      margin: getMargin(left: 16, right: 16, bottom: 4),
+                      child: Stack(
+                        alignment: Alignment.topLeft,
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Container(
+                              height: getVerticalSize(
+                                74.00,
+                              ),
+                              width: getHorizontalSize(
+                                396.00,
+                              ),
+                              margin: getMargin(
+                                top: 10,
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              const Text('Dont have account?'),
+                              TextButton(
+                                child: const Text(
+                                  'Sign in',
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => SignUpScreen(
+                                                name: widget.name,
+                                                email: widget.email,
+                                                phone: widget.phone,
+                                              )));
+                                },
+                              )
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
   }
 }
