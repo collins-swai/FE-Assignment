03939eb4fb065897e27ab62985010ee25bfda69f
Collins Swai <swailerc23@gmail.com>
2023-03-28 16:40:57 +0300

    Shared preferences helper

---
+++ lib/presentation/SharedPreference/shared_preference_helper.dart
@@ -0,0 +1,13 @@
+class ClientComplaint {
+  final String title;
+  final String description;
+  final String comment;
+  final String status;
+
+  ClientComplaint({
+    required this.title,
+    required this.description,
+    required this.comment,
+    required this.status,
+  });
+}
